717404a610fdbeb6a047732c51ab3f809b0bcddc
SarthakB07 <115055474+SarthakB07@users.noreply.github.com>
2023-09-14 22:15:41 +0530

    Update index.js

--- Components/index.js
+++ Components/index.js
@@ -1,1 +1,21 @@
-//Sarthak
+import Footer from "./Footer";
+import Table from "./Table";
+import NavBar from "./NavBar";
+import Form from "./Form";
+import Services from "./Services";
+import Profile from "./Profile";
+import GetShipment from "./GetShipment";
+import CompleteShipment from "./CompleteShipment";
+import StartShipment from "./StartShipment";
+// SVG
+export{
+Footer,
+Table,
+NavBar,
+Form,
+Services,
+Profile,
+GetShipment,
+CompleteShipment,
+StartShipment
+};
