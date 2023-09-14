a73ddc047e7899f1deac42c05e3560dada788df8
SarthakB07 <115055474+SarthakB07@users.noreply.github.com>
2023-09-15 00:08:33 +0530

    Update StartShipment.jsx

--- Components/StartShipment.jsx
+++ Components/StartShipment.jsx
@@ -23,7 +23,54 @@ export default ({ startModal, setStartModal, startShipment }) => {
                             <Str1/>
                         </button>
                     </div>
+                    <div className="max-w-sm mx-auto py-3 space-y-3 text-center">
+                      <h4 className="text-lg font-medium text-gray-800">
+                        Start The Shipping
+                      </h4>
+                      <form onSubmit={(e)=>e.preventDefault()}>
+                        <div className="relative mt-3">
+                          <input 
+                          type="text"
+                          placeholder="receiver"
+                          className="w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600
+                          shadow-sm rounded-lg"
+                          onChange={(e)=>
+                          setGetProduct({
+                            ...getProduct,
+                            reveiver: e.target.value,
+                          })
+                        }
+                        />
+                          </div>
+                          <div className="relative mt-3">
+                            <input
+                            type="text"
+                            placeholder="Id"
+                            className="w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600
+                            shadow-sm rounded-lg"
+                            onChange={(e)=>{
+                              setGetProduct({
+                                ...getProduct,
+                                index:e.target.value,
+                              })
+                            }
+                          }
+                            />
+                          
+                          </div>
+                          <button
+                          onClick={()=>startShiping()}
+                          className="block w-full mt-3 py-3 px-4 font-medium text-sm text-center text-white bg-indigo-600 hover:bg-indigo-500 
+                          active:bg-indigo-700 rounded-lg ring-offset-2 ring-indigo-600 focus:ring-2 "
+                          >
+                            Get Details
+                          </button>
+                          </form> 
+                    </div>
                 </div>
             </div>
         </div>
-}
+    ):(
+      ""
+    );
+};
